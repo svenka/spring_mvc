4f74db307da49387257d3deb358421f1badbd756
svenka <svenkagrip@gmail.com>
2023-05-03 22:02:51 +0800

    add @RequestParam("employeeName") to showEmployeeDetails

--- src/main/java/com/fyodork/spring/mvc/MyController.java
+++ src/main/java/com/fyodork/spring/mvc/MyController.java
@@ -3,6 +3,7 @@ package com.fyodork.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -27,10 +28,20 @@ public class MyController {
     {
         return "show-emp-details-view";
     }*/
-    @RequestMapping("/showDetails")
+    /*@RequestMapping("/showDetails")
     public String showEmployeeDetails(HttpServletRequest request, Model model)
     {
         String empName=request.getParameter("employeeName");
+        empName="Mr. "+empName;
+        model.addAttribute("nameAttribute",empName);
+        return "show-emp-details-view";
+    }*/
+
+    @RequestMapping("/showDetails")
+    public String showEmployeeDetails(@RequestParam("employeeName") String empName,
+                                      Model model)
+    {
+
         empName="Mr. "+empName;
         model.addAttribute("nameAttribute",empName);
         return "show-emp-details-view";

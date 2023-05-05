83ee18e15aa4935c4241b77780e3957e2746986a
svenka <svenkagrip@gmail.com>
2023-05-05 22:29:32 +0800

    add @RequestMapping("/employee/")

--- src/main/java/com/fyodork/spring/mvc/MyController.java
+++ src/main/java/com/fyodork/spring/mvc/MyController.java
@@ -8,13 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee/")
 public class MyController {
 
     @RequestMapping("/")
     public String showFirstView()
-    {
-
-        return "first-view";
+    { return "first-view";
     }
 
     @RequestMapping("/askDetails")
@@ -23,19 +22,6 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
-/*    @RequestMapping("/showDetails")
-    public String showEmployeeDetails()
-    {
-        return "show-emp-details-view";
-    }*/
-    /*@RequestMapping("/showDetails")
-    public String showEmployeeDetails(HttpServletRequest request, Model model)
-    {
-        String empName=request.getParameter("employeeName");
-        empName="Mr. "+empName;
-        model.addAttribute("nameAttribute",empName);
-        return "show-emp-details-view";
-    }*/
 
     @RequestMapping("/showDetails")
     public String showEmployeeDetails(@RequestParam("employeeName") String empName,
